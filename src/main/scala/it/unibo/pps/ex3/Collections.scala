bb3ce9c8bb413748a7114801dd33eb97d3592d3d
Samuele Ferri <samuele1301@gmail.com>
2026-04-18 15:21:07 +0200

    feat: Linear sequences done

--- src/main/scala/it/unibo/pps/ex3/Collections.scala
+++ src/main/scala/it/unibo/pps/ex3/Collections.scala
@@ -1,7 +1,7 @@
 package it.unibo.pps.ex3
 
 import java.util.concurrent.TimeUnit
-
+import scala.collection.mutable.ListBuffer
 import scala.concurrent.duration.FiniteDuration
 
 object PerformanceUtils:
@@ -20,6 +20,17 @@ object PerformanceUtils:
 @main def checkPerformance: Unit =
 
   /* Linear sequences: List, ListBuffer */
+  val list = List(1, 2, 3, 4, 5)
+  val secondElement = list(1)
+  val newList = list.updated(1, 99)
+  val deleteElementList = list.filter(element => element % 2 == 0)
+
+  val listBuffer = ListBuffer[Int](1, 2, 3, 4, 5)
+  val thirdElement = listBuffer(2)
+  listBuffer(2) = 99
+  listBuffer.filterInPlace(element => element % 3 == 0)
+  listBuffer.remove(0)
+
 
   /* Indexed sequences: Vector, Array, ArrayBuffer */
 
